dd749751bc28be74b5ec206f69194890a4c3ac75
sebastianmatto26-hue <sebastianmatto26@gmail.com>
2026-05-27 01:17:26 -0400

    si el primer commit

---
+++ Practica con ejercicios para el primer examen de Programación 1.py
@@ -0,0 +1,8 @@
+#Declara dos variables a = 7 y b = 3. Imprime su suma con un f-string.
+a = 7
+b = 3
+print(f"La suma de los números 7+3 es igual a {a + b}")
+
+#Pide el radio de un círculo (float). Calcula el área con math.pi * radio ** 2 y muestra el resultado.
+número_del_usuario = input("Ingrese el número para calcular el área: ")
+área = print(f"El radio del área es igual a = {número_del_usuario ** 2}")
---
+++ sebas_practica.py
@@ -0,0 +1,6 @@
+nombre_y_apellido_de_la_persona = input("Ingrese su nombre: ")
+print("Hola " + nombre_y_apellido_de_la_persona)
+print("Como estas, " + nombre_y_apellido_de_la_persona + "? ")
+
+bienestar_del_usuario = input("ingrese si esta bien o mal, por favor: ")
+print("Que bueno " + nombre_y_apellido_de_la_persona + "!")
